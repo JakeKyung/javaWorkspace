e6f5124eb846296a7f190ccd268a3e283df0e26e
Daehyun Kyung <cokolavel@gmail.com>
2023-10-27 16:03:25 +0900

    DataTypes

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println(123);
+        System.out.println(-123);
+        System.out.println(1.23);
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(123 + 345);
+    }
+}
