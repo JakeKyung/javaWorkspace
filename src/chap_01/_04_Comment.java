5b8f09b3b68d6494d4425aa3b3840636a0b2093e
Daehyun Kyung <cokolavel@gmail.com>
2023-10-27 16:49:09 +0900

    Comment

---
+++ src/chap_01/_04_Comment.java
@@ -0,0 +1,16 @@
+package chap_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+        // 주석 단축키 (Ctrl + /)
+
+        // 한줄 주석
+        int size = 120;
+        size = size + 10; // size up
+        System.out.println(size);
+
+        /*
+        여러줄 주석
+         */
+    }
+}
